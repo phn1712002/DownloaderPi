7a5cb824a74174872c3552ce47e01f50d34f7df3
HoanggNam <phn1712002@gmail.com>
2024-05-21 11:28:57 +0700

    feat: :white_check_mark: Cập nhật code

---
+++ Downloader.py
@@ -0,0 +1,14 @@
+from rclone_python import rclone
+import os, time
+from Json import load_json
+
+def main():
+  if rclone.is_installed():
+    config = load_json('./config.json')
+    while True:
+      os.system('clear')
+      rclone.sync(config['src_path'], ['dest_path'])
+      time.sleep(config['time_sleep'])
+
+if __name__ == '__main__':
+  main()
