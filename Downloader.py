30c9563874688a4be3f2c7ac7f96797304cf3543
HoanggNam <phn1712002@gmail.com>
2024-05-21 12:16:33 +0700

    feat: :lock: Cập nhật code

--- Downloader.py
+++ Downloader.py
@@ -3,11 +3,12 @@ import os, time
 from Json import load_json
 
 def main():
+  
   if rclone.is_installed():
     config = load_json('./config.json')
     while True:
       os.system('clear')
-      rclone.sync(config['src_path'], ['dest_path'])
+      rclone.sync(config['src_path'], config['dest_path'])
       time.sleep(config['time_sleep'])
 
 if __name__ == '__main__':
