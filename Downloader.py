ec8d5fbe644aa192ec84634c1599cc668226e668
HoanggNam <phn1712002@gmail.com>
2024-05-21 19:57:25 +0700

    feat: :white_check_mark: Cập nhật code

--- Downloader.py
+++ Downloader.py
@@ -1,19 +1,32 @@
 from rclone_python import rclone
-import os, time, telegram, datetime, asyncio
+import os, time, datetime, asyncio
 from Json import load_json
+import smtplib
+from email.mime.text import MIMEText
+
 os.system('pyclean .')
+
+def send_email(subject, body, sender, recipients, password):
+    msg = MIMEText(body)
+    msg['Subject'] = subject
+    msg['From'] = sender
+    msg['To'] = ', '.join(recipients)
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp_server:
+       smtp_server.login(sender, password)
+       smtp_server.sendmail(sender, recipients, msg.as_string())
+    print("Message sent!")
 
 def main():
   if rclone.is_installed():
     config = load_json('./config.json')
-    bot = telegram.Bot(token=config['api_key'])
+    subject = "DownloaderPi"
     
     while True:
       try:
         os.system('clear')
         rclone.sync(config['src_path'], config['dest_path'])
-        text_send_telegram = f"DownloaderPi synchronization successful at {get_datetime()}"
-        asyncio.run(send_mail(bot, config, text_send_telegram))
+        text_send = f"DownloaderPi synchronization successful at {get_datetime()}"
+        send_email(subject, text_send, config['sender'], config['recipients'], config['password'])
         time.sleep(config['time_sleep'])
       except KeyboardInterrupt:
         os.system('clear')
@@ -27,8 +40,6 @@ def get_datetime():
   formatted_datetime = current_datetime.strftime("%d-%m-%Y-%H-%M-%S")
   return formatted_datetime
 
-async def send_mail(bot, config, text_send_telegram):
-  return await bot.send_message(chat_id=config['chat_id'], text=text_send_telegram)
   
 if __name__ == '__main__':
   main()
