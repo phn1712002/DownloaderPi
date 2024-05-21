31c87b0340f264fe70aabc217b96deaa7e46647a
HoanggNam <phn1712002@gmail.com>
2024-05-21 12:34:11 +0700

    feat: :white_check_mark: Cập nhật code mới

--- Downloader.py
+++ Downloader.py
@@ -1,15 +1,29 @@
 from rclone_python import rclone
-import os, time
+import os, time, telegram, datetime, asyncio
 from Json import load_json
+os.system('pyclean .')
 
 def main():
-  
   if rclone.is_installed():
     config = load_json('./config.json')
+    bot = telegram.Bot(token=config['api_key'])
+    
     while True:
       os.system('clear')
       rclone.sync(config['src_path'], config['dest_path'])
+      text_send_telegram = f"DownloaderPi synchronization successful at {get_datetime()}"
+      asyncio.run(send_mail(bot, config, text_send_telegram))
       time.sleep(config['time_sleep'])
+     
 
+
+def get_datetime():
+  current_datetime = datetime.datetime.now()
+  formatted_datetime = current_datetime.strftime("%d-%m-%Y-%H-%M-%S")
+  return formatted_datetime
+
+async def send_mail(bot, config, text_send_telegram):
+  return await bot.send_message(chat_id=config['chat_id'], text=text_send_telegram)
+  
 if __name__ == '__main__':
   main()
